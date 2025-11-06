2dbf265aaa1aebdbe8aa3740f31d69447ea2d89a
mzz <3108482097@qq.com>
2025-11-06 19:24:15 +0800

    测试自己的py库导入

---
+++ mylib/src/mylib/__init__.py
@@ -0,0 +1,3 @@
+from .operations import add, multiply
+
+__version__ = "0.1.0"
---
+++ test_use_mymath.py
@@ -0,0 +1,7 @@
+# test_use_mymath.py
+
+import mylib
+
+print("Add:", mylib.add(3, 5))        # 输出: Add: 8
+print("Multiply:", mylib.multiply(2, 7))  # 输出: Multiply: 14
+print("Version:", mylib.__version__)
